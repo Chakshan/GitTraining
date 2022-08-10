40fa20fdef2c7345c08d4b100bd2351088e47c45
Alejandro <aqealejandro@gmail.com>
2022-08-10 16:44:14 -0700

    new java file

---
+++ Alejandro/Alex.java
@@ -0,0 +1,12 @@
+package Alejandro;
+
+public class Alex{
+    private int callDelay = 0;
+    private int distance = 0;
+    private String phoneNumber = "whatever";
+
+    public Alex(int callDelay, int distance) {
+        this.callDelay = callDelay;
+        this.distance = distance;
+    }
+}   
